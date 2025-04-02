4395093124dd09171badede7631afd134c4de5e8
monkuous <84319635+monkuous@users.noreply.github.com>
2025-04-02 21:12:41 +0200

    kernel: improve syscall logging

--- sources/andromeda-kernel/kernel/sys/syscall.c
+++ sources/andromeda-kernel/kernel/sys/syscall.c
@@ -5,44 +5,65 @@
 #include "sys/memory.h" /* IWYU pragma: keep */
 #include "sys/misc.h"   /* IWYU pragma: keep */
 #include "sys/thread.h" /* IWYU pragma: keep */
-#include "util/print.h"
 #include <andromeda/syscall.h>
 #include <errno.h>
 #include <stdint.h>
 
-#define LOG_SYSCALLS 1
+#define LOG_SYSCALLS_NONE 0
+#define LOG_SYSCALLS_ERROR 1
+#define LOG_SYSCALLS_ALL 2
+
+#define LOG_SYSCALLS LOG_SYSCALLS_NONE
 
 #if LOG_SYSCALLS
-static void log_pre_syscall(const char *name, int count, ...) {
-    printk("syscall: %s(", name);
+#include "util/print.h"
 
-    va_list args;
-    va_start(args, format);
+static void log_syscall_start(const char *name, int count, va_list args) {
+    printk("syscall: %s(", name);
 
     while (count) {
         printk("0x%x", va_arg(args, unsigned long));
         if (--count) printk(", ");
     }
 
-    va_end(args);
-
     printk(") ");
 }
 
-static int64_t log_post_syscall(int64_t value) {
-    if (value >= 0) {
+static void log_syscall_ret(int64_t value) {
+    if (likely(value >= 0)) {
         printk("-> 0x%X\n", value);
     } else {
         printk("-> %d\n", (int)value);
     }
+}
 
+static void log_pre_syscall([[maybe_unused]] const char *name, [[maybe_unused]] int count, ...) {
+#if LOG_SYSCALLS > LOG_SYSCALLS_ERROR
+    va_list args;
+    va_start(args, count);
+    log_syscall_start(name, count, args);
+    va_end(args);
+#endif
+}
+
+static int64_t log_post_syscall(int64_t value, [[maybe_unused]] const char *name, [[maybe_unused]] int count, ...) {
+#if LOG_SYSCALLS <= LOG_SYSCALLS_ERROR
+    if (likely(value >= 0)) return value;
+
+    va_list args;
+    va_start(args, count);
+    log_syscall_start(name, count, args);
+    va_end(args);
+#endif
+
+    log_syscall_ret((int)value);
     return value;
 }
 
 #define SYSWRAP(name, n, x, ...)                                                                                       \
     ({                                                                                                                 \
         log_pre_syscall(#name, n, __VA_ARGS__);                                                                        \
-        log_post_syscall(x(__VA_ARGS__));                                                                              \
+        log_post_syscall(x(__VA_ARGS__), #name, n, __VA_ARGS__);                                                       \
     })
 #else
 #define SYSWRAP(name, n, x, ...) (x(__VA_ARGS__))
