60e4c3ee689076a22cb4d6e3f9afdd472f64a4ce
monkuous <84319635+monkuous@users.noreply.github.com>
2025-04-03 17:27:51 +0200

    init: start bash in login mode

--- sources/andromeda-init/main.c
+++ sources/andromeda-init/main.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 int main(int, char *argv[]) {
-    char *args[] = {"/sbin/bash", nullptr};
+    char *args[] = {"/sbin/bash", "--login", nullptr};
     execve("/sbin/bash", args, environ);
     fprintf(stderr, "%s: failed to start bash: %m\n", argv[0]);
-    return 0;
+    return 127;
 }
