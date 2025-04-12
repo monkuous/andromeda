0c189553058c05ea28e235bc57d0c4f5fb0835af
monkuous <84319635+monkuous@users.noreply.github.com>
2025-04-12 16:28:25 +0200

    kernel: add support for gpt disks

--- sources/andromeda-kernel/kernel/drv/partition.c
+++ sources/andromeda-kernel/kernel/drv/partition.c
@@ -3,8 +3,10 @@
 #include "fs/pgcache.h"
 #include "fs/vfs.h"
 #include "mem/vmalloc.h"
+#include "string.h"
 #include "util/panic.h"
 #include "util/print.h"
+#include <stdint.h>
 
 typedef struct [[gnu::packed]] {
     uint8_t attr;
@@ -91,7 +93,161 @@ static int discover_mbr(bdev_t *bdev, const void *name, size_t length, part_cb_t
     return 1;
 }
 
-static discover_func_t discover_funcs[] = {
+typedef struct [[gnu::packed]] {
+    uint32_t a;
+    uint16_t b;
+    uint16_t c;
+    uint8_t d[8];
+} guid_t;
+
+typedef struct [[gnu::packed]] {
+    uint8_t signature[8];
+    uint32_t revision;
+    uint32_t header_size;
+    uint32_t header_checksum;
+    uint32_t reserved;
+    uint64_t header_lba;
+    uint64_t backup_lba;
+    uint64_t data_start_lba;
+    uint64_t data_end_lba;
+    guid_t guid;
+    uint64_t partitions_lba;
+    uint32_t num_partitions;
+    uint32_t entry_size;
+    uint32_t partitions_checksum;
+} gpt_header_t;
+
+typedef struct [[gnu::packed]] {
+    guid_t type;
+    guid_t guid;
+    uint64_t head_lba;
+    uint64_t tail_lba;
+    uint64_t attributes;
+    uint16_t name[36];
+} gpt_partition_t;
+
+uint32_t crc32(const void *data, size_t size) {
+    uint32_t crc = 0xFFFFFFFF;
+
+    while (size--) {
+        uint8_t byte = *(const uint8_t *)data++;
+        crc = crc ^ byte;
+
+        for (int i = 0; i < 8; i++) {
+            uint32_t mask = -(crc & 1);
+            crc = (crc >> 1) ^ (0xEDB88320 & mask);
+        }
+    }
+
+    return ~crc;
+}
+
+static int try_read_header(bdev_t *bdev, gpt_header_t *header, uint64_t block) {
+    int error = pgcache_read(&bdev->data, header, sizeof(*header), block << bdev->block_shift);
+    if (unlikely(error)) return -error;
+
+    if (memcmp(header->signature, "EFI PART", 8)) return 0;
+
+    uint32_t checksum = header->header_checksum;
+    header->header_checksum = 0;
+    uint32_t computed_checksum;
+
+    if (header->header_size <= sizeof(*header)) {
+        computed_checksum = crc32(header, header->header_size);
+    } else {
+        void *buffer = vmalloc(header->header_size);
+        error = pgcache_read(&bdev->data, buffer, header->header_size, block << bdev->block_shift);
+        if (unlikely(error)) {
+            vmfree(buffer, header->header_size);
+            return -error;
+        }
+        computed_checksum = crc32(buffer, header->header_size);
+        vmfree(buffer, header->header_size);
+    }
+
+    if (checksum != computed_checksum) {
+        printk("gpt: invalid checksum 0x%8x (got 0x%8x)\n", checksum, computed_checksum);
+        return 0;
+    }
+
+    return 1;
+}
+
+static void format_guid(unsigned char buffer[36], const guid_t *guid) {
+    snprintk(
+            buffer,
+            36,
+            "%8x-%4x-%4x-%2x%2x-%2x%2x%2x%2x%2x%2x",
+            guid->a,
+            guid->b,
+            guid->c,
+            guid->d[0],
+            guid->d[1],
+            guid->d[2],
+            guid->d[3],
+            guid->d[4],
+            guid->d[5],
+            guid->d[6],
+            guid->d[7]
+    );
+}
+
+static int discover_gpt(bdev_t *bdev, const void *name, size_t length, part_cb_t cb, void *ctx) {
+    static const guid_t EMPTY_GUID = {};
+
+    gpt_header_t header;
+    int error = try_read_header(bdev, &header, 1);
+    if (unlikely(error < 0)) return error;
+
+    if (!error) {
+        error = try_read_header(bdev, &header, bdev->blocks - 1);
+        if (unlikely(error < 0)) return error;
+        if (!error) return 0;
+    }
+
+    size_t parts_size = header.entry_size * header.num_partitions;
+    void *partitions = vmalloc(parts_size);
+
+    error = -pgcache_read(&bdev->data, partitions, parts_size, header.partitions_lba << bdev->block_shift);
+    if (unlikely(error)) goto exit;
+
+    uint32_t real_checksum = crc32(partitions, parts_size);
+    if (real_checksum != header.partitions_checksum) {
+        printk("gpt: invalid partition array checksum 0x%8x (got 0x%8x)\n", real_checksum, header.partitions_checksum);
+        goto exit;
+    }
+
+    // create id-based symlink
+    unsigned char id_buf[36];
+    format_guid(id_buf, &header.guid);
+
+    unsigned char buffer[64];
+    size_t link_length = snprintk(buffer, sizeof(buffer), "/dev/volumes/%S", id_buf, sizeof(id_buf));
+    ASSERT(link_length <= sizeof(buffer));
+
+    char *target;
+    size_t targ_len = asprintk(&target, "../%S", name, length);
+    error = -vfs_symlink(nullptr, buffer, link_length, target, targ_len);
+    vmfree(target, targ_len);
+    if (unlikely(error)) goto exit;
+
+    for (size_t i = 0; i < header.num_partitions; i++) {
+        gpt_partition_t *part = partitions + i * header.entry_size;
+        if (!memcmp(&part->type, &EMPTY_GUID, sizeof(EMPTY_GUID))) continue;
+
+        format_guid(id_buf, &part->guid);
+
+        cb(part->head_lba, part->tail_lba - part->head_lba + 1, id_buf, sizeof(id_buf), ctx);
+    }
+
+    error = 1;
+exit:
+    vmfree(partitions, parts_size);
+    return error;
+}
+
+static const discover_func_t discover_funcs[] = {
+        discover_gpt,
         discover_mbr,
 };
 
